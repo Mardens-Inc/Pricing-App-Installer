d573b72b29d06bfadb64220aa8ddd06ea9e661e2
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-02 13:16:26 -0400

    Add loading feedback during app installation
    The code now incorporates a loading feedback mechanism to improve the user experience during app installation. This is accomplished via the use of startLoading and stopLoading functions imported from loading.js. It also switches to an async promise-based approach to handle the fetching of default install location and API response.

--- src/assets/js/installer.js
+++ src/assets/js/installer.js
@@ -1,3 +1,5 @@
+import {startLoading, stopLoading} from "./loading.js";
+
 const installLocationInput = $("input#install-location");
 
 
@@ -13,7 +15,7 @@ $("#select-install-location").on("click", async () => {
 });
 
 $("#install").on("click", async () => {
-
+    startLoading({message: "Installing the app. Please wait..."});
     const location = installLocationInput.val();
     const createDesktopShortcut = $("toggle#create-desktop-shortcut").attr("value") === "true";
     const createStartMenuShortcut = $("toggle#create-start-menu-shortcut").attr("value") === "true";
@@ -25,15 +27,23 @@ $("#install").on("click", async () => {
         createStartMenuShortcut: createStartMenuShortcut,
         startWithWindows: startWithWindows
     });
+    await window.__TAURI__.invoke("start_application", {exe: `${location}/pricing-app.exe`});
+    stopLoading();
 });
 
-fetch("https://pricing-new.mardens.com/api/clients/versions").then(response => {
-    if (!response.ok) {
-        serverDown();
-    }
-}).catch(() => serverDown());
-
-const serverDown = () => {
-    $("main").html(`<h1 style="text-align: center;font-size: 5rem;">Server is down</h1>`)
-    $("#action-button-row").remove();
-};
+startLoading()
+Promise.all([
+    (async () => {
+        const location = await window.__TAURI__.invoke("get_default_install_location");
+        installLocationInput.val(location);
+    })(),
+    (async () => {
+        const response = await fetch("https://pricing-new.mardens.com/api/clients/versions");
+        if (!response.ok) {
+            $("main").html(`<h1 style="text-align: center;font-size: 5rem;">Server is down</h1>`)
+            $("#action-button-row").remove();
+        }
+    })()
+]).then(() => {
+    stopLoading();
+})
