3a54aa1efe0f7eb1b36b898ee0193bf907afe4e4
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-02 11:10:28 -0400

    Update main.rs for directory creation and permissions
    The main.rs file has been updated to handle directory creation and setting permissions. It now includes checks and actions for command line arguments, specifically for an option to create a directory. If this option is present, it also sets the appropriate permissions for the new directory. The Tauri invoke handler has also been updated to manage installation and application start-up.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,17 +1,40 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod installer;
+use std::{env, fs};
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use rclip_cmd::option::Option;
+use rclip_cmd::options_manager::OptionsManager;
+
+use crate::installer::{install, start_application};
+
+mod installer;
+mod process_utility;
 
 fn main() {
+    if env::args().len() > 1 {
+        let options = vec![
+            Option::new("d".to_string(), "create_directory".to_string(), false, true, "Creates the specified directory".to_string()),
+        ];
+        let mut options_manager = OptionsManager::new("Pricing App Installer", options);
+        if let Ok(_) = options_manager.parse_options(env::args().collect()) {
+            if options_manager.is_present("d") {
+                let directory = options_manager.argument("d");
+                fs::create_dir_all(&directory).unwrap();
+
+                if let Ok(mut child) = std::process::Command::new("cacls").args(&[&directory, "/t", "/e", "/g", "Everyone:f"]).spawn() {
+                    child.wait().unwrap();
+                } else {
+                    eprintln!("Failed to set directory permissions");
+                }
+            }
+        }
+        return;
+    }
+
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![ install, start_application])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
