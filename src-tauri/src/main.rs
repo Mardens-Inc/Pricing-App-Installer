74a2a79b959315192b4bcda1ac7e7bf80876b164
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-02 13:16:46 -0400

    Refactor main.rs to simplify and optimize code
    Removed unneeded code blocks and imports from main.rs file in src-tauri. In addition, added exit_application and get_default_install_location to the invoke handler to improve application flow. The update streamlines the app's installation process and removes unnecessary permission setting steps.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,40 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::{env, fs};
-
-use rclip_cmd::option::Option;
-use rclip_cmd::options_manager::OptionsManager;
-
-use crate::installer::{install, start_application};
+use crate::installer::{exit_application, get_default_install_location, install, start_application};
 
 mod installer;
-mod process_utility;
 
 fn main() {
-    if env::args().len() > 1 {
-        let options = vec![
-            Option::new("d".to_string(), "create_directory".to_string(), false, true, "Creates the specified directory".to_string()),
-        ];
-        let mut options_manager = OptionsManager::new("Pricing App Installer", options);
-        if let Ok(_) = options_manager.parse_options(env::args().collect()) {
-            if options_manager.is_present("d") {
-                let directory = options_manager.argument("d");
-                fs::create_dir_all(&directory).unwrap();
-
-                if let Ok(mut child) = std::process::Command::new("cacls").args(&[&directory, "/t", "/e", "/g", "Everyone:f"]).spawn() {
-                    child.wait().unwrap();
-                } else {
-                    eprintln!("Failed to set directory permissions");
-                }
-            }
-        }
-        return;
-    }
-
-
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![ install, start_application])
+        .invoke_handler(tauri::generate_handler![ install, start_application, get_default_install_location, exit_application])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
